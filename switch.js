60ba82d15096ca35bf1b7158cf46732f5d13d9cc
Dastar123 <raromar2014@gmail.com>
2023-06-07 17:58:20 +0000

    Final version

--- switch.js
+++ switch.js
@@ -1,3 +1,7 @@
+/*Obtiene el elemento con el ID "light-text" que muestra el texto actual del interruptor.
+Si el texto es "OFF", lo cambia a "ON". Si el texto es "ON", lo cambia a "OFF".
+Agrega o elimina la clase CSS "lights-on" en el elemento body, lo que cambia el color de fondo de la página.
+*/
 function change(){
     var x=document.getElementById('light-text');
     console.log("entrando en el chage");
